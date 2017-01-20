7b1dc114c8233b722c9e3116f7b82a5deac7a455
Artus <artus91@ukr.net>
2017-01-20 10:59:04 +0200

    The removing data created

--- app/src/main/java/leveleight/artus/com/myapplication/MainActivity.java
+++ app/src/main/java/leveleight/artus/com/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package leveleight.artus.com.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ import org.androidannotations.annotations.EditorAction;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -30,11 +33,12 @@ import java.util.Map;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    private static final String VERSION_DB = "9";
+
     private static final String QUERY_ALL = "query_all";
     private static final String QUERY_INTEGER = "query_integer";
     private static final String QUERY_SELECT_INTERVAL = "query_more_than";
-    private static final String QUERY_START = "query_between";
-    private static final String QUERY_TILL = "query_till";
+    private static final String QUERY_DELETE_ALL = "query_delete_all";
 
     private CouchDatabase mCouchDb;
 
@@ -46,6 +50,96 @@ public class MainActivity extends AppCompatActivity {
 
     @ViewById(R.id.edittext_end_label)
     protected TextView mEndLabel;
+
+    @ViewById(R.id.editRemove)
+    protected EditText mEditRemoveById;
+
+    @Click(R.id.removeAllBtn)
+    protected void handleRemoveAll() {
+        Query query = mCouchDb.mDatabase.getView(QUERY_ALL).createQuery();
+        try {
+            QueryEnumerator result = query.run();
+            for (Iterator<QueryRow> it = result; it.hasNext(); ) {
+                QueryRow row = it.next();
+                boolean deleted = row.getDocument().delete();
+                if (!deleted) {
+                    throw new RuntimeException("Not deleted all, why?");
+                }
+            }
+        } catch (CouchbaseLiteException aE) {
+            aE.printStackTrace();
+        }
+    }
+
+    @Click(R.id.removeByNumberBtn)
+    protected void handleRemoveById() {
+        String txt = mEditRemoveById.getText().toString();
+        if (Ints.tryParse(txt) != null) {
+            int count = delete(Ints.tryParse(txt));
+            showMessage("Deleted is ", count);
+        } else {
+            showMessage("The invalid number");
+        }
+        mEditRemoveById.setText("");
+    }
+
+    private void showMessage(Object... aMessage) {
+        Toast.makeText(this, Joiner.on(" ").join(Arrays.asList(aMessage)), Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Gets count of deleted
+     *
+     * @param aNumber Number to delete
+     * @return Count of deleted document
+     */
+    private int delete(int aNumber) {
+        Query query = mCouchDb.mDatabase.getView(QUERY_INTEGER).createQuery();
+        query.setKeys(Arrays.asList(aNumber));
+
+        int count = 0;
+        try {
+            query.setKeys(Arrays.asList(aNumber));
+            QueryEnumerator result = query.run();
+            for (Iterator<QueryRow> it = result; it.hasNext(); ) {
+                QueryRow row = it.next();
+                boolean deleted = row.getDocument().delete();
+                if (!deleted) {
+                    throw new RuntimeException("Not deleted, why?");
+                }
+                count++;
+            }
+        } catch (CouchbaseLiteException aE) {
+            aE.printStackTrace();
+        }
+        return count;
+    }
+
+    @EditorAction(R.id.editRemove)
+    void handleRemoveEditAction(TextView aTxt, int action, KeyEvent aEvent) {
+        handleRemoveById();
+    }
+
+    @EditorAction(R.id.edittext_save_label)
+    void handleEditAction(TextView aTxt, int action, KeyEvent aEvent) {
+        String txt = aTxt.getText().toString();
+        save(txt);
+        aTxt.setText("");
+    }
+
+    @EditorAction(R.id.edittext_end_label)
+    void handleEndAction(TextView aTxt, int action, KeyEvent aEvent) {
+        handleSelectInterval();
+    }
+
+    @EditorAction(R.id.edittext_start_label)
+    void handleStartAction(TextView aTxt, int action, KeyEvent aEvent) {
+        mEndLabel.requestFocus();
+    }
+
+    @ViewById(R.id.removeAllBtn)
+    protected Button mRemoveBtn;
+
 
     @Click(R.id.showAllBtn)
     protected void handleShowAllBtn() {
@@ -79,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
                 aQuery.setStartKey(start);
             });
         } else {
-            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+            showMessage("Wrong");
         }
         print(list);
     }
@@ -121,23 +215,6 @@ public class MainActivity extends AppCompatActivity {
             aE.printStackTrace();
         }
         return list;
-    }
-
-    @EditorAction(R.id.edittext_save_label)
-    void handleEditAction(TextView aTxt, int action, KeyEvent aEvent) {
-        String txt = aTxt.getText().toString();
-        save(txt);
-        aTxt.setText("");
-    }
-
-    @EditorAction(R.id.edittext_end_label)
-    void handleEndAction(TextView aTxt, int action, KeyEvent aEvent) {
-        handleSelectInterval();
-    }
-
-    @EditorAction(R.id.edittext_start_label)
-    void handleStartAction(TextView aTxt, int action, KeyEvent aEvent) {
-        mEndLabel.requestFocus();
     }
 
     private void print(List aList) {
@@ -192,15 +269,15 @@ public class MainActivity extends AppCompatActivity {
         public void execute(Database aDatabase) {
             View query = aDatabase.getView(QUERY_ALL);
             query.setMap((document, emitter) ->
-                    emitter.emit(document.get(SimpleItem.DATA), "nothing"), "1");
+                    emitter.emit(document.get(SimpleItem.DATA), "nothing"), VERSION_DB);
 
             query = aDatabase.getView(QUERY_INTEGER);
             query.setMap((document, emitter) -> {
                 String str = (String) document.get(SimpleItem.DATA);
                 if (Ints.tryParse(str) != null) {
-                    emitter.emit(str, "nothing");
+                    emitter.emit(Ints.tryParse(str), "nothing");
                 }
-            }, "1");
+            }, VERSION_DB);
 
             query = aDatabase.getView(QUERY_SELECT_INTERVAL);
             query.setMap((document, emitter) -> {
@@ -208,7 +285,15 @@ public class MainActivity extends AppCompatActivity {
                 if (Ints.tryParse(str) != null) {
                     emitter.emit(Ints.tryParse(str), "nothing");
                 }
-            }, "6");
+            }, VERSION_DB);
+
+            query = aDatabase.getView(QUERY_DELETE_ALL);
+            query.setMap((document, emitter) -> {
+                String str = (String) document.get(SimpleItem.DATA);
+                if (Ints.tryParse(str) != null) {
+                    emitter.emit(Ints.tryParse(str), "nothing");
+                }
+            }, VERSION_DB);
         }
     };
 }
